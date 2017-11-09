ce064ddfeffad6d61d64fa85cb390703f83c4c62
sandeep.kulkarni <sandeep.kulkarni@hotmail.com>
2017-11-09 10:26:12 -0500

    Added Sub Function

--- FirstGitApp/FirstGitApp/Program.cs
+++ FirstGitApp/FirstGitApp/Program.cs
@@ -13,6 +13,8 @@ namespace FirstGitApp
             Console.WriteLine("Hello World!");
 
             Console.WriteLine("Sum of 10, 20 is "+ add(10,20));
+            Console.WriteLine("Diff of 10, 20 is " + sub(10, 20));
+
             Console.ReadKey();
         }
 
@@ -20,5 +22,10 @@ namespace FirstGitApp
         {
             return x + y;
         }
+        static int sub(int x, int y)
+        {
+            return x - y;
+        }
     }
 }
+
